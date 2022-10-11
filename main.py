a065d7ae44911169a73ec87618e112e2c87318b6
besugos <besugos@gmail.com>
2022-10-11 15:39:40 -0300

    feat: initialize fastAPI

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import uvicorn
+from fastapi import FastAPI
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"msg": "Hello World!"}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    uvicorn.run("main:app", host='127.0.0.1', port=8000, log_level="info", reload=True)
+    print("running")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+

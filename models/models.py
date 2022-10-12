c53e8f2922e8dec1872e19f1027aedfe0dc81673
besugos <besugos@gmail.com>
2022-10-12 17:27:09 -0300

    feat: completed models

--- models/models.py
+++ models/models.py
@@ -1,13 +1,39 @@
 from typing import Union
 
 from pydantic import BaseModel
+from pydantic.schema import datetime, time, date
+from pydantic.types import date
 
 
 class Employee(BaseModel):
     employee_id: int = None
     first_name: str
+    last_name: str
+    cpf: str
+    email: str
+    password: str
+    zipcode: str
+    birthdate: date
+    street: str
+    number: str
+    district: str
+    city: str
+    state: str
+    children_amount: int
+    children_names: str
+    marital_status: str
 
 
 class Dayoff(BaseModel):
     dayoff_id: int = None
     employee_id: int
+    date: date
+    type: str
+    start: time
+    end: time
+    communicated_peers: bool
+    communicated_team: bool
+    communicated_manager: bool
+    has_substitute: bool
+    substitute: str
+    pending_work: str

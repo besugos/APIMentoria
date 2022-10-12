15de76def39df12572f9b78d67e19fbf275dbb61
besugos <besugos@gmail.com>
2022-10-12 17:37:43 -0300

    feat: added db methods

--- persistency/persistency_utils.py
+++ persistency/persistency_utils.py
@@ -1,9 +1,28 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 
-def get_session():
-    engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/postgres")
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    return session
+engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/postgres")
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+
+# def get_session():
+#     engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/postgres")
+#     Session = sessionmaker(bind=engine)
+#     session = Session()
+#     return session
+
+
+def create_db():
+    Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
+
